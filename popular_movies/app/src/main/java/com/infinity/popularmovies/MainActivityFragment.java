202dd9ca32e5629677a7e8752ff2b564bc69490f
Kiran Babu <kiran.united@gmail.com>
2015-12-15 14:29:24 +0530

    Improved implementation of Movie grid display

--- popular_movies/app/src/main/java/com/infinity/popularmovies/MainActivityFragment.java
+++ popular_movies/app/src/main/java/com/infinity/popularmovies/MainActivityFragment.java
@@ -10,13 +10,8 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivityFragment extends Fragment {
@@ -24,7 +19,6 @@ public class MainActivityFragment extends Fragment {
     private final String logTag = getClass().getSimpleName();
 
     private List<Movie> movieList = null;
-    private List<String> movieThumbnailPaths = new ArrayList<>();
     private RecyclerView.Adapter imageAdapter = null;
     private IntentFilter intentFilter = new IntentFilter();
 
@@ -32,19 +26,9 @@ public class MainActivityFragment extends Fragment {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
-
             if (MovieDataFetchService.REPLY_FETCH_MOVIE_LIST_UPDATE.equals(action)) {
-                movieThumbnailPaths.clear();
-
                 movieList = intent.getParcelableArrayListExtra("movie_list");
-                for (Movie movie : movieList) {
-                    String imageURL = movie.getPosterThumbnail();
-                    /*Log.v(logTag, movie.getTitle() + " " +
-                            imageURL + " " + movie.getReleaseDate().toString());*/
-                    movieThumbnailPaths.add(imageURL);
-                }
-
-                ((ImageViewAdapter)imageAdapter).updateImageDataSet(movieThumbnailPaths);
+                ((ImageViewAdapter)imageAdapter).updateImageDataSet(movieList);
                 imageAdapter.notifyDataSetChanged();
             }
         }
@@ -56,32 +40,14 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.list_view_movies);
-        //GridView gridView = (GridView) rootView.findViewById(R.id.grid_view);
         if (recyclerView == null) {
             Log.v(logTag, "No GridView instance available in Fragment");
             return rootView;
         }
 
         intentFilter.addAction(MovieDataFetchService.REPLY_FETCH_MOVIE_LIST_UPDATE);
-        imageAdapter = new ImageViewAdapter(getActivity(), movieThumbnailPaths);
+        imageAdapter = new ImageViewAdapter(getActivity(), movieList);
         recyclerView.setAdapter(imageAdapter);
-        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
-
 
         return rootView;
     }
@@ -89,9 +55,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).registerReceiver(receiver,
-                intentFilter);
+        LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).registerReceiver(
+                receiver, intentFilter);
 
+        Log.v(logTag, getActivity().getIntent().getAction() + "; " + getActivity().getIntent().getStringExtra("back_key"));
         MainActivity parent = (MainActivity)getActivity();
         MovieDataFetchService.sendActionRequest(this.getActivity(), MovieDataFetchService.ACTION_FETCH_MOVIE_LIST,
                 parent.getMovieSortOrder());
@@ -100,6 +67,8 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+        if (movieList != null)
+            movieList.clear();
         LocalBroadcastManager.getInstance(this.getActivity().getApplicationContext()).unregisterReceiver(receiver);
     }
 
